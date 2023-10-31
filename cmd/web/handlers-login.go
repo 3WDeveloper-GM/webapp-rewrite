20cd7c8a1a3c9be5a6ae2998c55a97872561d781
3WDeveloper-GM <glopezmad@gmail.com>
2023-10-31 11:10:20 -0600

    31/10/2023 3rd commit of the day, made some routes and created the handlers responsible for signups

--- cmd/web/handlers-login.go
+++ cmd/web/handlers-login.go
@@ -5,17 +5,45 @@ import (
 	"net/http"
 
 	config "github.com/3WDeveloper-GM/webapp-rewrite/cmd/pkg/configuration"
+	"github.com/3WDeveloper-GM/webapp-rewrite/internal"
 )
+
+type userSignupForm struct {
+	Name     string `form:"name"`
+	Email    string `form:"email"`
+	Password string `form:"password"`
+	internal.Validator
+}
 
 func userSignup(app *config.Application) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Some web form for signup")
+		data := app.GetTemplateData(r)
+		data.Form = userSignupForm{}
+		app.Render(w, http.StatusOK, "signup.tmpl", data)
 	}
 }
 
 func userSignupPost(app *config.Application) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Some method limited for posting")
+		var formdata userSignupForm
+
+		err := app.DecodePostForm(r, &formdata)
+		if err != nil {
+			app.ClientError(w, http.StatusBadRequest)
+			return
+		}
+
+		formdata.CheckField(internal.NotBlank(formdata.Name), "name", "Field cannot be blank")
+		formdata.CheckField(internal.NotBlank(formdata.Email), "email", "Field cannot be blank")
+		formdata.CheckField(internal.Matches(formdata.Email, internal.EmailRX), "email", "this field must be a valid email address")
+		formdata.CheckField(internal.MinChars(formdata.Password, 8), "password", "This field cannot be less than 8 characters")
+		formdata.CheckField(internal.NotBlank(formdata.Password), "password", "field cannot be blank")
+
+		if !formdata.Valid() {
+			data := app.GetTemplateData(r)
+			data.Form = formdata
+			app.Render(w, http.StatusOK, "signup.tmpl", data)
+		}
 	}
 }
 
--- go.mod
+++ go.mod
@@ -11,4 +11,5 @@ require (
 	github.com/alexedwards/scs/v2 v2.6.0 // indirect
 	github.com/go-playground/form/v4 v4.2.1 // indirect
 	github.com/justinas/alice v1.2.0 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
 )
