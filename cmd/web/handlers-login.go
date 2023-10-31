81a99f1c6738ee90867265be0cb7371521d804bd
3WDeveloper-GM <glopezmad@gmail.com>
2023-10-31 11:52:26 -0600

    31/10/2023 4th commit of the day, refactored

--- cmd/web/handlers-login.go
+++ cmd/web/handlers-login.go
@@ -7,14 +7,14 @@ import (
 
 	config "github.com/3WDeveloper-GM/webapp-rewrite/cmd/pkg/configuration"
 	"github.com/3WDeveloper-GM/webapp-rewrite/internal/models"
-	internal "github.com/3WDeveloper-GM/webapp-rewrite/internal/validator"
+	validator "github.com/3WDeveloper-GM/webapp-rewrite/internal/validator"
 )
 
 type userSignupForm struct {
 	Name     string `form:"name"`
 	Email    string `form:"email"`
 	Password string `form:"password"`
-	internal.Validator
+	validator.Validator
 }
 
 func userSignup(app *config.Application) http.HandlerFunc {
@@ -36,12 +36,12 @@ func userSignupPost(app *config.Application) http.HandlerFunc {
 		}
 
 		//Form validation, I start to notice the pattern.
-		formdata.CheckField(internal.NotBlank(formdata.Name), "name", "Field cannot be blank")
-		formdata.CheckField(internal.NotBlank(formdata.Email), "email", "Field cannot be blank")
-		formdata.CheckField(internal.Matches(formdata.Email, internal.EmailRX), "email", "this field must be a valid email address")
-		formdata.CheckField(internal.MinChars(formdata.Password, 8), "password", "This field cannot be less than 8 characters")
-		formdata.CheckField(internal.NotBlank(formdata.Password), "password", "field cannot be blank")
-		formdata.CheckField(internal.MaxChars(formdata.Password, 30), "password", "Field cannot be more than 30 characters.")
+		formdata.CheckField(validator.NotBlank(formdata.Name), "name", "Field cannot be blank")
+		formdata.CheckField(validator.NotBlank(formdata.Email), "email", "Field cannot be blank")
+		formdata.CheckField(validator.Matches(formdata.Email, validator.EmailRX), "email", "this field must be a valid email address")
+		formdata.CheckField(validator.MinChars(formdata.Password, 8), "password", "This field cannot be less than 8 characters")
+		formdata.CheckField(validator.NotBlank(formdata.Password), "password", "field cannot be blank")
+		formdata.CheckField(validator.MaxChars(formdata.Password, 30), "password", "Field cannot be more than 30 characters.")
 
 		if !formdata.Valid() {
 			data := app.GetTemplateData(r)
